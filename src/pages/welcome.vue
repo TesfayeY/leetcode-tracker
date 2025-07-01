796caacddae35e31315b4504743cd378a3f77975
Tesfa <tesfayeyadesakefene@gmail.com>
2025-07-01 14:48:49 -0500

    Updated welcome page used as landing page.
    Welcome page now includes, ceate group and join group.
    Logout.post.ts updated to display a message after successful logout
    Layout.default updated to include dropdown menu

--- src/pages/welcome.vue
+++ src/pages/welcome.vue
@@ -1,25 +1,66 @@
-<template>
-  <div>
-    <h1 class="font-default text-xl flex justify-center pt-5">Welcome, {{ name }}</h1>
-  </div>
-</template>
-
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
 import { ref } from 'vue';
-import { useRouter } from 'vue-router';
+// Import your modal components
+import JoinGroupModal from '~/components/JoinGroupModal.vue';
+import CreateGroupModal from '~/components/CreateGroupModal.vue';
 
 definePageMeta({
-        layout: 'default',
-        middleware: 'require-auth'
-    })
+  layout: 'default',
+  middleware: 'require-auth'
+});
 
-// console.log(useCookie('name'))
 const displayName = useCookie('name').value;
-// console.log('Name:', displayName);
+const userName = ref<string | null>(displayName);
 
+const router = useRouter()
 
-//Define the name ref
-const name = ref<string | null>(displayName);
-const router = useRouter();
+// Reactive variables to control modal visibility
+const showCreateGroupModal = ref(false);
+const showJoinGroupModal = ref(false);
 
+function openCreateGroupModal() {
+  showCreateGroupModal.value = true;
+}
+
+function closeCreateGroupModal() {
+  showCreateGroupModal.value = false;
+}
+
+function openJoinGroupModal() {
+  showJoinGroupModal.value = true;
+}
+
+function closeJoinGroupModal() {
+  showJoinGroupModal.value = false;
+}
 </script>
+
+<template>
+  <div class="min-h-screen flex items-center justify-center px-4">
+    <div class="text-center">
+      <h1 class="text-3xl font-semibold mb-8">Welcome, {{ userName }}!</h1>
+
+      <button
+        @click="openCreateGroupModal"
+        class="w-48 py-3 mb-4 rounded-lg shadow-md
+               bg-green-600 text-white font-medium
+               hover:bg-blue-700 transition"
+      >
+        Create Group
+      </button>
+
+      <button
+        @click="openJoinGroupModal"
+        class="w-48 py-3 mb-4 rounded-lg shadow-md
+               bg-green-600 text-white font-medium border 
+               hover:bg-blue-700 transition"
+      >
+        Join Group
+      </button>
+    </div>
+
+    <CreateGroupModal v-if="showCreateGroupModal" @close="closeCreateGroupModal" />
+    <JoinGroupModal v-if="showJoinGroupModal" @close="closeJoinGroupModal" />
+  </div>
+</template>
