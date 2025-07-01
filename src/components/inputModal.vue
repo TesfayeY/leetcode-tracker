a72cca34ff837194f2140fc03b478231504c6ba5
Tan Nguyen <tnguyen@bioneos.com>
2025-07-01 10:45:32 -0500

    Add input popup modal to enter and receive input

---
+++ src/components/inputModal.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="fixed top-0 bottom-0 left-0 right-0 flex justify-center items-center bg-[#000000da]">
+    <div class="modal rounded-lg bg-[#363636] to-10% p-5 h-fit w-[500px] text-left">
+      <h1 class="text-left text-lg ">Add Leetcode Username</h1>
+      <UFormGroup class="mt-5">
+        <UInput v-model="inputField" type="text"/>
+      </UFormGroup>
+      <div class="mt-5 flex flex-end gap-4">
+        <UButton class="p-2 px-3 text-md" type="submit" @click="sendFormToParent">Submit</UButton>
+        <UButton class="p-2 px-3 text-md" @click="closeModal">Close</UButton>
+      </div>
+    </div>
+  </div>
+  
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const inputField = ref('');
+const emit = defineEmits(['submit-form', 'close-modal']);
+
+// These function will emit the control and value back to the parent 
+function sendFormToParent() {
+  emit('submit-form', inputField.value);
+}
+
+function closeModal() {
+  emit('close-modal')
+}
+
+</script>
+
+<style scoped>
+</style>
--- src/pages/users/[username].vue
+++ src/pages/users/[username].vue
@@ -1,20 +1,32 @@
 <template>
-  <div v-if="!hasLeetcodeProfile.value" class="flex flex-col place-content-evenly h-[60vh]">
+  <div v-if="!hasLeetcodeProfile" class="flex flex-col place-content-evenly h-[60vh]">
     <p class="text-center text-3xl">There is no profile to display</p>
-    <UButton class="p-5 text-lg">Add Leetcode Profile</UButton>
+    <UButton class="p-5 text-lg" @click="isInputModalOpen = true">Add Leetcode Profile</UButton>
+    <InputModal 
+      v-show="isInputModalOpen" 
+      @close-modal="isInputModalOpen = false" 
+      @submit-form="handleInputFormModal"
+    ></InputModal>
   </div>
+  <div v-else class="flex flex-col place-content-evenly h-[60vh]">
+    <p class="text-center text-3xl">Leetcode Profile: {{ lcUsername }}</p>
+  </div>
+  
 </template>
 
 <script lang="ts" setup>
 import { useRoute } from 'vue-router';
 import { ref, onMounted } from 'vue';
 import { useErrorLogger } from '../../composables/useErrorLogger';
+import InputModal from '../../components/inputModal.vue';
 
 const { reportError } = useErrorLogger();
 const route = useRoute();
 const username = ref(route.params.username);
 const token = useCookie('token') ;
 const hasLeetcodeProfile = ref(false);
+const isInputModalOpen = ref(false);
+const lcUsername = ref('');
 
 // Fetch the account data to check for leetcode username
 try {
@@ -28,9 +40,19 @@ try {
 
   if (response.success) {
     hasLeetcodeProfile.value = response.data.lcUsername !== "";
+    lcUsername.value = response.data.lcUsername;
   }
 
 } catch(error: any) {
   reportError(error,{ section : `users/${username.value}`})
+}
+
+//Handle value from input modal
+function handleInputFormModal(modalValue: string) {
+  if (modalValue !== "") {
+    lcUsername.value = modalValue;
+    hasLeetcodeProfile.value = true;
+    isInputModalOpen.value = false;
+  }
 }
 </script>
