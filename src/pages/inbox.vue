297b0c6edffdcf8e46374ccfaa80487be4aa0b35
Tan Nguyen <tnguyen@bioneos.com>
2025-07-08 16:30:45 -0500

    Add inbox filtering via searching

--- src/pages/inbox.vue
+++ src/pages/inbox.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="flex flex-row justify-start gap-5 h-[5vh] items-center">
-      <UButton class="p-2 px-3 text-md h-full" to="/welcome">Return</UButton>
-    </div>
+    <UButton class="p-2 px-3 text-md h-full" to="/welcome">Return</UButton>
+  </div>
   <UCard class="mt-5">
     <template #header>
-      <div class="flex flex-row justify-start gap-4">
+      <div class="flex flex-row justify-start items-center gap-5">
         <p class="text-left text-3xl">Inbox</p>
+        <UInput v-model="searchQuery" placeholder="Filter message..." />
       </div>
     </template>
     <div class="h-[400px]">
-      <UTable :rows="displayRows" :columns="columns" :empty-state="{ icon: 'i-heroicons-inbox', label: 'No inbox' }">
+      <UTable :rows="filteredRows" :columns="columns" :empty-state="{ icon: 'i-heroicons-inbox', label: 'No inbox' }">
         <template #actions-data="{ row }">
           <UDropdown :items="actionItems(row)">
             <UButton color="gray" icon="i-heroicons-ellipsis-horizontal-20-solid"></UButton>
@@ -27,12 +28,13 @@
 
 <script setup lang="ts">
 import { ref, onBeforeMount } from 'vue';
+import { NUM_INBOX_DEFAULT_DISPLAY } from '~/constants/appConst';
 import { useErrorLogger } from '~/composables/useErrorLogger';
 
 const { reportError }= useErrorLogger();
 const token = useCookie('token');
 const page = ref(1);
-const pageCount = 5;
+const pageCount = NUM_INBOX_DEFAULT_DISPLAY; 
 
 const inboxes = reactive([
   {
@@ -68,8 +70,19 @@ const columns = ref([
   }
 ]);
 
-const displayRows = computed(() => {
-  return toRaw(inboxes.value).slice((page.value - 1) * pageCount, (page.value) * pageCount)
+const searchQuery = ref('')
+const filteredRows = computed(() => {
+  if (!searchQuery.value) {
+    return toRaw(inboxes.value).slice((page.value - 1) * pageCount, (page.value) * pageCount);
+  }
+
+  const filter = toRaw(inboxes.value).filter((inbox) => {
+    return Object.values(inbox).some((value) => {
+      return String(value).toLowerCase().includes(searchQuery.value.toLowerCase());
+    });
+  });
+
+  return filter.slice((page.value - 1) * pageCount, (page.value) * pageCount);
 });
 
 const actionItems = (row: any) => [
@@ -127,7 +140,8 @@ const handleAcceptInvitation = (inbox: any) => {
 const handleReadInbox = async (inbox: any) => {
   const requestBody = {
     acknowledgement: 'VIEWED'
-  }
+  };
+  
   await fetchUserInbox('POST', inbox, requestBody);
 }
 
@@ -160,5 +174,4 @@ async function fetchUserInbox(method: 'POST' | 'GET' | 'PUT' | 'DELETE', inbox: 
     reportError(error, { section : `inbox`});
   }
 }
-
 </script>
