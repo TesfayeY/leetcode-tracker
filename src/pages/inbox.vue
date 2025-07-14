452b4784bf23a01ea61d23a7f2ce1268ae917253
Tan Nguyen <tnguyen@bioneos.com>
2025-07-14 11:32:27 -0500

    Add pagemeta on require auth on several new views

--- src/pages/inbox.vue
+++ src/pages/inbox.vue
@@ -89,6 +89,11 @@ import { NUM_INBOX_DEFAULT_DISPLAY } from '~/constants/appConst';
 import { useErrorLogger } from '~/composables/useErrorLogger';
 import ConfirmationModal from '~/components/confirmationModal.vue';
 
+definePageMeta({
+  layout: 'default',
+  middleware: 'require-auth'
+});
+
 const { reportError }= useErrorLogger();
 const token = useCookie('token');
 const page = ref(1);
